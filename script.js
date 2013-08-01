16524a12d4a7717f70acc41de80c03d9832f7327
damodawg <damianwylie@gmail.com>
2013-08-01 16:17:04 -0700

    jQuery file

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+        $('#button').click(function(){
+        var toAdd = $('input[name=checkListItem]').val();
+
+        $('.list').prepend('<div class="item">' + toAdd + '</div>');
+
+        });
+    $(document).on('click','.item', function(){
+        $(this).remove();
+    });
+});
